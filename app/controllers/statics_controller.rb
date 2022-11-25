057791062cd251be3b62fc983e30f2721eb0991b
ThomD <thomasnoidion@gmail.com>
2022-11-25 11:26:09 -0500

    last commit

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,8 +1,2 @@
 class StaticsController < ApplicationController
-	def index
-	end
-	def contact
-	end
-	def logan
-	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "about", to: "about#index"
+  get "about", to: "statics#index"
   # Defines the root path route ("/")
   root to: "statics#index"
 
   get "contact", to: "statics#contact"
   get 'shop', to: "statics#shop"
+  get 'mission', to: "statics#mission"
 end
